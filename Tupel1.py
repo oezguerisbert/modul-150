1d47d2fe8116f5c2e5b180704e2cf34f5edf9144
Özgür Isbert <oezguerisbert@gmail.com>
2021-03-10 10:25:42 +0100

    first python file

---
+++ Tupel1.py
@@ -0,0 +1,11 @@
+zahlen = (2, 3, 5, 7, 11)
+print(zahlen)
+person = ("Hans","Dampf", 45, True, None)
+print(person)
+tupel1 = (zahlen,person)
+print(tupel1)
+tupel2 = ((1,2),(3,4))
+print(tupel2)
+
+for i in tupel2:
+    print(i)
